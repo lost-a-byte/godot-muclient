2f9ba60b83612e5afe3e0da5437c671539825a57
Your Name <you@example.com>
2026-02-14 23:41:39 +0700

    Update extension

--- Extensions/VectorExtensions.cs
+++ Extensions/VectorExtensions.cs
@@ -1,34 +1,44 @@
 using System;
+using Godot;
 using MuClient.Models.Terrain;
 
 namespace MuClient.Extensions;
 
 public static class VectorExtensions
 {
-    public static Godot.Vector3 ToGodotVector3(this System.Numerics.Vector3 vector)
+    public static Vector3 ToGodotVector3(this System.Numerics.Vector3 vector)
     {
-        return new Godot.Vector3(vector.X, vector.Y, vector.Z);
+        return new Vector3(vector.X, vector.Y, vector.Z);
     }
-    public static Godot.Vector2 ToGodotVector2(this System.Numerics.Vector2 vector)
+    public static Vector2 ToGodotVector2(this System.Numerics.Vector2 vector)
     {
-        return new Godot.Vector2(vector.X, vector.Y);
+        return new Vector2(vector.X, vector.Y);
     }
-    public static Godot.Quaternion ToGodotQuaternion(this System.Numerics.Quaternion quaternion)
+    public static Quaternion ToGodotQuaternion(this System.Numerics.Quaternion quaternion)
     {
-        return new Godot.Quaternion(
+        return new Quaternion(
             quaternion.X,
             quaternion.Y,
             quaternion.Z,
             quaternion.W
         );
     }
-    public static TilePosition ToTilePosition(this Godot.Vector2 vector)
+    public static TilePosition ToTilePosition(this Vector2 vector)
     {
         return new TilePosition((byte)Math.Floor(vector.X + Constants.TerrainSize / 2), (byte)Math.Floor(vector.Y + Constants.TerrainSize / 2));
     }
-    public static TilePosition ToTilePosition(this Godot.Vector3 vector)
+    public static TilePosition ToTilePosition(this Vector3 vector)
     {
         return new TilePosition((byte)Math.Floor(vector.X + Constants.TerrainSize / 2), (byte)Math.Floor(vector.Z + Constants.TerrainSize / 2));
+    }
+    public static Vector2I GetXZTileVector2I(this Vector3 vector)
+    {
+        return new((byte)Math.Floor(vector.X + Constants.TerrainSize / 2), (byte)Math.Floor(vector.Z + Constants.TerrainSize / 2));
+    }
+
+    public static Vector2I GetXZTileVector2I(this Vector2 vector)
+    {
+        return new((byte)Math.Floor(vector.X + Constants.TerrainSize / 2), (byte)Math.Floor(vector.Y + Constants.TerrainSize / 2));
     }
 
 }
