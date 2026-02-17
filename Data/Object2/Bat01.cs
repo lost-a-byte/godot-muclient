bfb5b34b477aa3fec04d7013ad6e165ecfa2a662
Your Name <you@example.com>
2026-02-17 17:31:12 +0700

    init world 2 animations

---
+++ Data/Object2/Bat01.cs
@@ -0,0 +1,8 @@
+using Godot;
+using MuClient.Scenes.WorldObjects;
+using System;
+
+namespace MuClient.Data.Object2;
+public partial class Bat01 : AnimatedWorldObject
+{
+}
---
+++ Data/Object2/Object54.cs
@@ -0,0 +1,9 @@
+using Godot;
+using MuClient.Scenes.WorldObjects;
+using System;
+
+namespace MuClient.Data.Object2;
+
+public partial class Object54 : AnimatedWorldObject
+{
+}
