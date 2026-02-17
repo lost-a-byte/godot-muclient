7566ca5d2b355d84d313cdac5d7173ee2bcdb531
Your Name <you@example.com>
2026-02-17 15:22:12 +0700

    reindent move command file

--- Scenes/Controls/MoveCommand.cs
+++ Scenes/Controls/MoveCommand.cs
@@ -11,69 +11,69 @@ namespace MuClient.Scenes.Controls;
 [Tool]
 public partial class MoveCommand : Control
 {
-	[Export] 
-	public WorldType World { get; set; } = WorldType.LORENCIA;
+    [Export]
+    public WorldType World { get; set; } = WorldType.LORENCIA;
 
-	[Signal]
-	public delegate void MoveEventHandler(int world);
-	[Signal]
-	public delegate void MoveGateEventHandler(GateItem gate);
+    [Signal]
+    public delegate void MoveEventHandler(int world);
+    [Signal]
+    public delegate void MoveGateEventHandler(GateItem gate);
 
-	private VBoxContainer? container;
+    private VBoxContainer? container;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		container = GetNode<VBoxContainer>("Window/ScrollContainer/List");
-		BuildWorldList();
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        container = GetNode<VBoxContainer>("Window/ScrollContainer/List");
+        BuildWorldList();
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 
-	void BuildWorldList()
-	{
-		ClearWorldList();
+    void BuildWorldList()
+    {
+        ClearWorldList();
 
-		MprData mprData = ResourceLoader.Load<MprData>("res://Data/Lang.mpr");
-		Array<GateItem> gateItems = mprData.GetGateItems();
+        MprData mprData = ResourceLoader.Load<MprData>("res://Data/Lang.mpr");
+        Array<GateItem> gateItems = mprData.GetGateItems();
 
-		foreach (GateItem gate in gateItems)
-		{
-			if (gate.Type == 2 || gate.Type == 1)
-			{
-				continue;
-			}
-			Button button = new()
-			{
-				Text = $"{gate.World} {gate.PositionStart}",
-			};
-			button.Pressed += () => GatePressed(gate);
-			container?.AddChild(button);
-		}
-	}
+        foreach (GateItem gate in gateItems)
+        {
+            if (gate.Type == 2 || gate.Type == 1)
+            {
+                continue;
+            }
+            Button button = new()
+            {
+                Text = $"{gate.World} {gate.PositionStart}",
+            };
+            button.Pressed += () => GatePressed(gate);
+            container?.AddChild(button);
+        }
+    }
 
-	void ClearWorldList()
-	{
-		if (container == null) return;
-		foreach (var item in container.GetChildren())
-		{
-			item.QueueFree();
-		}
-	}
+    void ClearWorldList()
+    {
+        if (container == null) return;
+        foreach (var item in container.GetChildren())
+        {
+            item.QueueFree();
+        }
+    }
 
-	private void GatePressed(GateItem gateItem)
-	{
-		GetViewport().SetInputAsHandled();
-		EmitSignal(SignalName.MoveGate, gateItem);
-	}
+    private void GatePressed(GateItem gateItem)
+    {
+        GetViewport().SetInputAsHandled();
+        EmitSignal(SignalName.MoveGate, gateItem);
+    }
 
-	private void ButtonPressed(WorldType world)
-	{
-		GD.Print($"World {world} clicked!");
-		GetViewport().SetInputAsHandled();
-		EmitSignal(SignalName.Move, (int)world);
-	}
+    private void ButtonPressed(WorldType world)
+    {
+        GD.Print($"World {world} clicked!");
+        GetViewport().SetInputAsHandled();
+        EmitSignal(SignalName.Move, (int)world);
+    }
 }
